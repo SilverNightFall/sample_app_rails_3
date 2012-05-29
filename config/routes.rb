6d30a49f2654394f80890552f1da299057af57af
Victoria Bagnell <victoria@jbyte.com>
2012-05-29 19:20:02 -0400

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

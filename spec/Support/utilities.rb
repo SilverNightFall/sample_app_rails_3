6d5cb71bb75ae35d65aedec5be4346fd3c2c4f9b
Victoria Bagnell <victoria@jbyte.com>
2012-06-02 14:59:43 -0400

    update pages

--- spec/Support/utilities.rb
+++ spec/Support/utilities.rb
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
+
 include ApplicationHelper
-=======
+
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -9,4 +9,4 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
->>>>>>> 48428d724c1060235aff5fb4a9448fda519520df
+

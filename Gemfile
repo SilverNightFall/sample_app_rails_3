07926958a98f60fa4e8365f897c07a3bca82c00b
Victoria Bagnell <victoria@jbyte.com>
2012-06-07 01:48:09 -0400

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ source 'https://rubygems.org'
     gem 'therubyracer'
     gem 'mysql2'
     gem 'bcrypt-ruby', '3.0.1'
+    gem 'faker', '1.0.1'
+    gem 'will_paginate', '3.0.3'
+    gem 'bootstrap-will_paginate', '0.0.6'
+
 
 
 group :development, :test do
--- spec/Support/utilities.rb
+++ spec/Support/utilities.rb
@@ -1,4 +1,3 @@
-
 include ApplicationHelper
 
 def full_title(page_title)
@@ -10,3 +9,10 @@ def full_title(page_title)
   end
 end
 
+def sign_in(user)
+	visit signin_path
+	fill_in "Email", with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	cookies[:remember_token] = user.remember_token
+end
